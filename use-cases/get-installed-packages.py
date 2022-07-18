d9f4ec5c9125d2c5ffcfaee5d97528e24c93d4f0
Charlie Guo <qlguo@hotmail.com>
2022-07-18 13:03:16 -0400

    .

--- use-cases/get-installed-packages.py
+++ use-cases/get-installed-packages.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import salesforce_scripts
+import json
 
 (instance, token) = salesforce_scripts.get_instance_and_access_token()
-assert salesforce_scripts.list_installed_packages(instance, token)
+print(json.dumps(salesforce_scripts.list_installed_packages(instance, token), indent=4))
--- use-cases/query-users.py
+++ use-cases/query-users.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import salesforce_scripts
+import json
 
-def test_query_users():
-    (instance, token) = salesforce_scripts.get_instance_and_access_token()
-    assert salesforce_scripts.query_users(instance, token)
+(instance, token) = salesforce_scripts.get_instance_and_access_token()
+print(json.dumps(salesforce_scripts.query_users(instance, token), indent=4))
