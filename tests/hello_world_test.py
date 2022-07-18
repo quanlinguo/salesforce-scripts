42585724796fae9a60f729dd15ab112d28f21b06
Charlie Guo <qlguo@hotmail.com>
2022-07-18 11:59:23 -0400

    pytest

--- tests/hello_world_test.py
+++ tests/hello_world_test.py
@@ -2,7 +2,7 @@ import salesforce_scripts
 
 def test_hello_world():
     assert not salesforce_scripts.hello_world()
-    ### Assert that the function does not return anything. 
+    ''' Assert that the function does not return anything. '''
 
 def test_get_hello_world():
     assert salesforce_scripts.get_hello_world() == 'Hello World!'
--- tests/installed_package_test.py
+++ tests/installed_package_test.py
@@ -4,5 +4,6 @@ def test_list_installed_packages():
     (instance, token) = salesforce_scripts.get_instance_and_access_token()
     assert salesforce_scripts.list_installed_packages(instance, token)
 
-    ### Shun a more obscure version
-    # assert salesforce_scripts.list_installed_packages(*salesforce_scripts.get_instance_and_access_token())
+    ''' Shun a more obscure version
+    assert salesforce_scripts.list_installed_packages(*salesforce_scripts.get_instance_and_access_token())
+    '''
