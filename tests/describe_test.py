3854bfc21f7fb270a5b44eb2c700296d4cf6d3e9
Charlie Guo <qlguo@hotmail.com>
2022-07-19 08:04:44 -0400

    .
    Remove due to a bug: descirbe object can miss fields

--- tests/describe_test.py
+++
@@ -1,9 +0,0 @@
-import salesforce_scripts
-
-def test_list_objects():
-    (instance, token) = salesforce_scripts.get_instance_and_access_token()
-    assert salesforce_scripts.list_objects(instance, token)
-
-def test_describe_object():
-    (instance, token) = salesforce_scripts.get_instance_and_access_token()
-    assert salesforce_scripts.describe_object(instance, token, 'Account')
--- use-cases/describe-object.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import salesforce_scripts
-import json
-
-(instance, token) = salesforce_scripts.get_instance_and_access_token()
-custom_object = salesforce_scripts.describe_object(instance, token, "Account")
-for field in custom_object["fields"]:
-    print("%s\t|\t%s\t|\t%s" % (field['fullName'], field['label'], field['type']))
